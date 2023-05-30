c992b7f32927a8f519672629875ee7a5e72bd3d3
adiyoga <adiyoga27new@gmail.com>
2023-05-30 13:18:25 +0800

    update

--- app/Jobs/SendWhatsapJob.php
+++ app/Jobs/SendWhatsapJob.php
@@ -45,15 +45,10 @@ class SendWhatsapJob implements ShouldQueue
                 if ($file->type == 'images') {
 
                     $fileType = 'image';
-                    $val =  url('storage').'/'.$file->url;
                 } elseif ($file->type == 'videos') {
                     $fileType = 'video';
-                    // $val = 'https://rr4---sn-npoe7ns7.googlevideo.com/videoplayback?expire=1676554079&ei=_trtY_u-O6KDvdIPqI2p0As&ip=143.244.54.120&id=o-AMDJX9R2M4xGIhllLdyz1YRMOm1YNcBbAW_tkL4EY1Cw&itag=18&source=youtube&requiressl=yes&spc=H3gIhm1kVa-mp0JC6WCd4NYFfpTOSZM&vprv=1&mime=video%2Fmp4&ns=2uQ7rGqwelno2hDA29UUMnQL&cnr=14&ratebypass=yes&dur=205.357&lmt=1665524362101341&fexp=24007246&c=WEB&txp=5538434&n=LLCP5GyItW-KlA&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Cmime%2Cns%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgc9h_YinhQyR7FgWI5cOJ9ZaeQQBqhWF_4WCxlwg00q8CIQCeIFNOocYGxagHfelnQKsaix_iZTMX3YmKoVbD3EQztA%3D%3D&rm=sn-c0qs776&req_id=6ddb409d621fa3ee&ipbypass=yes&redirect_counter=2&cm2rm=sn-jcopn2-q2ns7e&cms_redirect=yes&cmsv=e&mh=T8&mip=101.128.99.153&mm=29&mn=sn-npoe7ns7&ms=rdu&mt=1676531827&mv=m&mvi=4&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRAIgHNYA-UcdRTLursozFbNj_NSx2xV7n1zudrlT0l21B1ICIHE3n0kqOtAsazeX22IHdWj8NZ5Fu8z-c4EAUOZuzJ9j';
-                    $val =  url('storage').'/'.$file->url;
                 } elseif ($file->type == 'file') {
                     $fileType = 'file';
-                    // $val = "http://eprints.uny.ac.id/62836/2/BAB%202.pdf";
-                    $val =  url('storage').'/'.$file->url;
                 }
 
                 $response =  Http::timeout(10)->withHeaders([
@@ -67,7 +62,6 @@ class SendWhatsapJob implements ShouldQueue
                         'url' => url('storage') . '/' . ($file->url),
                         // 'url' => $val
                     ]);
-                    dd($response);
             }
 
 
