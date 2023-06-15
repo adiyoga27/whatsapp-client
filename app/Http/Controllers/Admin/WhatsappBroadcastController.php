027e4ca3ba6381f9597d50f2c74d8a6a718701cd
Adiyoga <adiyoga27new@gmail.com>
2023-06-15 18:01:24 +0800

    sff

--- app/Http/Controllers/Admin/WhatsappBroadcastController.php
+++ app/Http/Controllers/Admin/WhatsappBroadcastController.php
@@ -39,15 +39,15 @@ class WhatsappBroadcastController extends Controller
         })->catch(function (Batch $batch, Throwable $th) {
             // First batch job failure detected...
             (new BotTelegram)->error($th);
-            $message = Message::where('batch_id', $batch->id)->first();
-            QueueMessage::where('status', 'progress')->where('message_id', $message->id)->update([
-                'status'=>'pending'
-            ]);
+            // $message = Message::where('batch_id', $batch->id)->first();
+            // QueueMessage::where('status', 'progress')->where('message_id', $message->id)->update([
+            //     'status'=>'pending'
+            // ]);
 
 
             
-            (new BotTelegram)->info('catch : First batch job failure detected '.$batch->id);
-            $batch->delete();
+            // (new BotTelegram)->info('catch : First batch job failure detected '.$batch->id);
+            // $batch->delete();
 
         })->finally(function (Batch $batch) {
             // The batch has finished executing...
